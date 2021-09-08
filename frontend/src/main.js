583bce6651b69c2e0b1242800deff13087989e4a
Timo Hanke <timo.t.hanke@gmail.com>
2021-09-08 14:21:42 +0200

    use auto-generated key_sync.did.js in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,8 +16,36 @@ const vaultCanister = "uvf7r-liaaa-aaaah-qabnq-cai";
 
 let authClient;
 
-var myKeyPair = "";
-var myPublicKey = "";
+export const keySync_idlFactory = ({ IDL }) => {
+  const PublicKey = IDL.Text;
+  const Ciphertext = IDL.Text;
+  const GetCiphertextError = IDL.Variant({
+    'notSynced' : IDL.Null,
+    'notFound' : IDL.Null,
+  });
+  const Result = IDL.Variant({ 'ok' : Ciphertext, 'err' : GetCiphertextError });
+  const DeviceAlias = IDL.Text;
+  const KeySync = IDL.Service({
+    'get_ciphertext' : IDL.Func([PublicKey], [Result], ['query']),
+    'get_devices' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(DeviceAlias, PublicKey))],
+        ['query'],
+      ),
+    'get_unsynced_pubkeys' : IDL.Func([], [IDL.Vec(PublicKey)], ['query']),
+    'isSeeded' : IDL.Func([], [IDL.Bool], ['query']),
+    'register_device' : IDL.Func([DeviceAlias, PublicKey], [IDL.Bool], []),
+    'remove_device' : IDL.Func([DeviceAlias], [], ['oneway']),
+    'seed' : IDL.Func([PublicKey, Ciphertext], [], ['oneway']),
+    'submit_ciphertexts' : IDL.Func(
+        [IDL.Vec(IDL.Tuple(PublicKey, Ciphertext))],
+        [],
+        ['oneway'],
+      ),
+    'whoami' : IDL.Func([], [IDL.Principal], ['query']),
+  });
+  return KeySync;
+};
 
 function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint8Array(buf));
@@ -64,16 +92,9 @@ init();
 whoamiBtn.addEventListener('click', async () => {
   const identity = await authClient.getIdentity();
 
-  // We either have an Agent with an anonymous identity (not authenticated),
-  // or already authenticated agent, or parsing the redirect from window.location.
-  const idlFactory = ({ IDL }) =>
-    IDL.Service({
-      whoami: IDL.Func([], [IDL.Principal], ['update']),
-    });
-
   const canisterId = Principal.fromText(keySyncCanister);
 
-  const actor = Actor.createActor(idlFactory, {
+  const actor = Actor.createActor(keySync_idlFactory, {
     agent: new HttpAgent({
       host: "https://ic0.app/",
       identity,
